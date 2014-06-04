63dfac78c2ebfeaf2e174edb2c1f9b0a7e9d3c8b
Raphael <raphael.lisboa@gmail.com>
2014-06-04 09:25:06 -0300

    init2

---
+++ src/main/java/br/com/caelum/vraptor/blank/HelloController.java
@@ -0,0 +1,11 @@
+package br.com.caelum.vraptor.blank;
+
+import br.com.caelum.vraptor.Controller;
+
+@Controller
+public class HelloController {
+
+	public String hello(){
+		return "ola mundo";
+	}
+}
